602a253ab2ba7e8706ab98a975ce7d35eb8c8070
Yoo Wonsang <dnjstkd04@naver.com>
2025-10-27 16:26:08 +0900

    Initial commit

---
+++ src/main/java/com/multi/y2k4/Y2k4Application.java
@@ -0,0 +1,13 @@
+package com.multi.y2k4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Y2k4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Y2k4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/multi/y2k4/Y2k4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.y2k4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Y2k4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
