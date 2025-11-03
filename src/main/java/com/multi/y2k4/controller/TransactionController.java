f57272d8dcc0181789330c5c6840bfac68af2e92
Yoo Wonsang <dnjstkd04@naver.com>
2025-11-04 03:14:53 +0900

    11/03일자 마무리

--- src/main/java/com/multi/y2k4/controller/TransactionController.java
+++ src/main/java/com/multi/y2k4/controller/TransactionController.java
@@ -1,6 +1,7 @@
 package com.multi.y2k4.controller;
 
 
+import com.multi.y2k4.vo.transaction.Purchase;
 import com.multi.y2k4.vo.transaction.Sale;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.ui.Model;
@@ -19,10 +20,10 @@ import java.util.List;
 public class TransactionController {
 
     @GetMapping("/sale/list")
-    public List<Sale> list(@RequestParam(required = false) Integer emp_id,
+    public List<Sale> saleList(@RequestParam(required = false) Integer emp_id,
                        @RequestParam(required = false) Integer client_id,
                        @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate order_date,
-                       @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate del_date,
+                       @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate due_date,
                        @RequestParam(required = false) String status,
                        Model model) {
 
@@ -43,7 +44,45 @@ public class TransactionController {
         sale2.setStatus("good");
         saleList.add(sale2);
 
+        System.out.println(emp_id);
+        System.out.println(client_id);
+        System.out.println(order_date);
+        System.out.println(due_date);
+        System.out.println(status);
         return saleList;
+    }
+
+    @GetMapping("/purchase/list")
+    public List<Purchase> purchaseList(@RequestParam(required = false) Integer emp_id,
+                                       @RequestParam(required = false) Integer client_id,
+                                       @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate order_date,
+                                       @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate del_date,
+                                       @RequestParam(required = false) String status,
+                                       Model model) {
+
+        //테스트를 위한 수동 리스트 값 생성
+        List<Purchase> purchaseList = new ArrayList<>();
+        Purchase purchase1 = new Purchase();
+        purchase1.setEmp_id(2);
+        purchase1.setClient_id(22);
+        purchase1.setOrder_date(LocalDate.parse("2025-11-04"));
+        purchase1.setDel_date(LocalDate.parse("2025-11-04"));
+        purchase1.setStatus("bad");
+        purchaseList.add(purchase1);
+        Purchase purchase2 = new Purchase();
+        purchase2.setEmp_id(2);
+        purchase2.setClient_id(22);
+        purchase2.setOrder_date(LocalDate.parse("2025-11-03"));
+        purchase2.setDel_date(LocalDate.parse("2025-11-03"));
+        purchase2.setStatus("bad");
+        purchaseList.add(purchase2);
+
+        System.out.println(emp_id);
+        System.out.println(client_id);
+        System.out.println(order_date);
+        System.out.println(del_date);
+        System.out.println(status);
+        return purchaseList;
     }
 
 }
--- src/main/resources/static/js/transaction.js
+++ src/main/resources/static/js/transaction.js
@@ -61,13 +61,20 @@ export async function sale_list(formData){
                             </thead>
                         `;
 
+    let tbody;
     const data = await $.ajax({
         url: '/api/transaction/sale/list',
         method: 'GET',
         dataType: 'json',
+        data:{
+            emp_id : emp_id,
+            client_id : client_id,
+            order_date : order_date,
+            due_date : due_date,
+            status : status,
+        }
 
     });
-    let tbody;
     tbody= `<tbody>`;
     console.log(data);
     $.each(data, function (i, row){
@@ -79,31 +86,147 @@ export async function sale_list(formData){
                         <td>${row.status}</td>
                         </tr>`
     })
-    tbody += `</tbody></table>`;
-    table += `</tbody></table>
-    <input type="button" data-file="transaction" data-fn="addSale" data-action="add">`;
-    return table;
+    tbody += `</tbody></table><input type="button" data-action="add" data-file="transaction" data-fn="addSale" value="추가">`;
+
+
+    console.log(table)
+    return table+tbody;
 }
 
-export function purchase_listAll(){
+export async function purchase_listAll(){
+
+    let table = `<table>
+                            <thead>
+                            <tr>
+                            <th>담당자</th>
+                            <th>거래처</th>
+                            <th>주문 수주일</th>
+                            <th>납기일</th>
+                            <th>현 상태</th>
+                            
+                            </tr>
+                            </thead>
+                        `;
+
+    let tbody;
+    const data = await $.ajax({
+        url: '/api/transaction/purchase/list',
+        method: 'GET',
+        dataType: 'json',
+
+
+    });
+    tbody= `<tbody>`;
+    console.log(data);
+    $.each(data, function (i, row){
+        tbody += `<tr>
+                        <td>${row.emp_id}</td>
+                        <td>${row.client_id}</td>
+                        <td>${row.order_date}</td>
+                        <td>${row.del_date}</td>
+                        <td>${row.status}</td>
+                        </tr>`
+    })
+    tbody += `</tbody></table><input type="button" data-action="add" data-file="transaction" data-fn="addPurchase" value="추가">`;
+
+
+    console.log(table)
+    return table+tbody;
 
 }
 
-export function purchase_list(formData){
+export async function purchase_list(formData){
+    const emp_id = formData.emp_id;     //담당자
+    const client_id = formData.client_id;       //거래처
+    const order_date = formData.order_date;     //주문일
+    const del_date = formData.del_date;         //요청 납기일
+    const status = formData.status;             //현상태
+
+    let table = `<table>
+                            <thead>
+                            <tr>
+                            <th>담당자</th>
+                            <th>거래처</th>
+                            <th>주문 수주일</th>
+                            <th>납기일</th>
+                            <th>현 상태</th>
+                            
+                            </tr>
+                            </thead>
+                        `;
+
+    let tbody;
+    const data = await $.ajax({
+        url: '/api/transaction/purchase/list',
+        method: 'GET',
+        dataType: 'json',
+        data:{
+            emp_id : emp_id,
+            client_id : client_id,
+            order_date : order_date,
+            del_date : del_date,
+            status : status,
+        }
+
+    });
+    tbody= `<tbody>`;
+    console.log(data);
+    $.each(data, function (i, row){
+        tbody += `<tr>
+                        <td>${row.emp_id}</td>
+                        <td>${row.client_id}</td>
+                        <td>${row.order_date}</td>
+                        <td>${row.del_date}</td>
+                        <td>${row.status}</td>
+                        </tr>`
+    })
+    tbody += `</tbody></table><input type="button" data-action="add" data-file="transaction" data-fn="addPurchase" value="추가">`;
+
+
+    console.log(table)
+    return table+tbody;
 
 }
 
-export function search_form(){
-    const search_bar = `<form data-file="transaction" data-fn="sale_list">
-                                <input type="text" name="emp_id" value="" />
-                                <input type="text" name="client_id" value="" />
-                                <input type="hidden" name="_token" value="" />
-                                <input type="date" name="order_date"  />
-                                <input type="date" name="due_date"  />
-                                <input type="button" data-file="transaction" data-fn="sale_list" value = "검색">
-                                </form>`
+export function  sale_search_form(){
+    return `<form data-file="transaction" data-fn="sale_list">
+                                <label> 담당자:
+                                    <input type="text" name="emp_id" value="" />
+                                </label>
+                                <label> 거래처:
+                                    <input type="text" name="client_id" value="" />
+                                </label>
+                                <label> 주문일:
+                                    <input type="date" name="order_date"  />
+                                </label>
+                                <label> 요청 납기일:
+                                    <input type="date" name="due_date"  />
+                                </label>
+                                    <label> 상태:
+                                <input type="button" data-file="transaction" data-fn="sale_list" data-action = "search" value = "검색">
+                                </label>
+                                </form>`;
 
-    return search_bar;
+}
+
+export function  purchase_search_form(){
+    return `<form data-file="transaction" data-fn="purchase_list">
+                                <label> 담당자:
+                                    <input type="text" name="emp_id" value="" />
+                                </label>
+                                <label> 거래처:
+                                    <input type="text" name="client_id" value="" />
+                                </label>
+                                <label> 주문 수주일:
+                                    <input type="date" name="order_date"  />
+                                </label>
+                                <label> 납기일:
+                                    <input type="date" name="del_date"  />
+                                </label>
+                                    <label> 상태:
+                                <input type="button" data-file="transaction" data-fn="purchase_list" data-action = "search" value = "검색">
+                                </label>
+                                </form>`;
 
 }
 
